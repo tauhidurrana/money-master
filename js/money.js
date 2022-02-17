4647a7ed13e4cec86c171c7c006fe710ecadd810
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-02-17 21:25:08 +0600

    code updated

--- js/money.js
+++ js/money.js
@@ -14,7 +14,12 @@ document.getElementById('calculate-expense').addEventListener('click',function()
     updateExpense()
 })
 
+document.getElementById('income-input').addEventListener('click',function(){
+    updateExpense()
+})
+
 function updateExpense(){
+    // Total Expense Calculation
     const foodInput = document.getElementById('food-expense').value;
     const rentInput = document.getElementById('rent-expense').value;
     const clothInput = document.getElementById('cloth-expense').value;
@@ -25,4 +30,7 @@ function updateExpense(){
     const total = document.getElementById('total-expense');
     total.innerText = totalExpenseCalculation;
 
+    const incomeInputText = document.getElementById('income-input').value;
+    const incomeInput = parseFloat(incomeInputText);
+    
 }
