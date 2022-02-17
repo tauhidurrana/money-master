4dc29bcfa005d83dc170a1d3331f9b2c740c42a2
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-02-17 20:44:21 +0600

    calculation first part added and button reload problem solved

--- js/money.js
+++ js/money.js
@@ -1,6 +1,28 @@
 document.getElementById('food-expense').addEventListener('click',function(){
-    console.log('hello');
-
-    const foodInput = document.getElementById('food-expense').value;
-    
+    updateExpense()
 })
+
+document.getElementById('rent-expense').addEventListener('click',function(){
+    updateExpense()
+})
+
+document.getElementById('cloth-expense').addEventListener('click',function(){
+    updateExpense()
+})
+
+document.getElementById('calculate-expense').addEventListener('click',function(){
+    updateExpense()
+})
+
+function updateExpense(){
+    const foodInput = document.getElementById('food-expense').value;
+    const rentInput = document.getElementById('rent-expense').value;
+    const clothInput = document.getElementById('cloth-expense').value;
+    const totalExpenseCalculation = 
+    parseFloat(foodInput) +
+    parseFloat(rentInput) +
+    parseFloat(clothInput);
+    const total = document.getElementById('total-expense');
+    total.innerText = totalExpenseCalculation;
+
+}
