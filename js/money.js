93c95103f1082f855b256cde150f3f2f83135253
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-02-18 16:18:15 +0600

    code updated with error handling and minimised event and organised again

--- js/money.js
+++ js/money.js
@@ -1,29 +1,9 @@
-document.getElementById('food-expense').addEventListener('click',function(){
-    updateExpense()
-})
-
-document.getElementById('rent-expense').addEventListener('click',function(){
-    updateExpense()
-})
-
-document.getElementById('cloth-expense').addEventListener('click',function(){
-    updateExpense()
-})
-
 document.getElementById('calculate-expense').addEventListener('click',function(){
     updateExpense()
 })
 
-document.getElementById('income-input').addEventListener('click',function(){
-    updateExpense()
-})
-
-document.getElementById('percent-value').addEventListener('click',function(){
-    updateExpense()
-})
-
 document.getElementById('save-button').addEventListener('click',function(){
-    updateExpense()
+    updateSaving();
 })
 
 function updateExpense(){
@@ -31,37 +11,60 @@ function updateExpense(){
     const foodInput = document.getElementById('food-expense').value;
     const rentInput = document.getElementById('rent-expense').value;
     const clothInput = document.getElementById('cloth-expense').value;
-    const ExpenseCalculation = 
-    parseFloat(foodInput) +
-    parseFloat(rentInput) +
-    parseFloat(clothInput);
+
+    const incomeInputText = document.getElementById('income-input').value;
+    const incomeInput = parseFloat(incomeInputText);
+    // validation
+    if (isNaN(incomeInputText) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothInput)) {
+        alert('Please insert positive numbers!');
+        return;
+    }
+
+    if (incomeInputText < 0 || foodInput < 0 || rentInput < 0 || clothInput < 0) {
+        alert('Wrong Input');
+        return;
+    }
+
+    if (incomeInputText == '' || foodInput == '' || rentInput == '' || clothInput == '') {
+        alert('Wrong Input');
+        return;
+    }
+
+    const ExpenseCalculation = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+    const totalExpense = parseFloat(ExpenseCalculation);
+
+    let balance = incomeInput - totalExpense;
+
     const total = document.getElementById('total-expense');
     total.innerText = ExpenseCalculation;
-    
-    // income & balance calculation
-    const incomeInputText = document.getElementById('income-input').value;
-    let incomeInput = parseFloat(incomeInputText);
-    const totalExpense = parseFloat(ExpenseCalculation);
-    
-    let balance = incomeInput - totalExpense;
+
+    // income & balance display
     const balanceTotal = document.getElementById('balance-total')
-    
     balanceTotal.innerText = balance;
 
+}
+
+function updateSaving() {
+    const incomeInputText = document.getElementById('income-input').value;
+    const incomeInput = parseFloat(incomeInputText);
     // percent value calculation
     const percentInputText = document.getElementById('percent-value').value;
+    // validation 
+    if (percentInputText == '' || percentInputText < 0 || isNaN(percentInputText)) {
+        alert('Please enter valid number');
+        return;
+    }
     let percentInput = parseFloat(percentInputText);
     const savingAmount = (incomeInput/100) * percentInput;
     const savingAmountOutput =  document.getElementById('saving-value');
     savingAmountOutput.innerText = savingAmount;
 
     // Remaining Balance
+    const balanceText = document.getElementById('balance-total').innerText;
+    const balance = parseFloat(balanceText);
 
     const remainingBalance = balance - savingAmount;
     const remaingBalanceOutput = document.getElementById('remaining-value');
 
     remaingBalanceOutput.innerText = remainingBalance;
-
-    
-    
 }
